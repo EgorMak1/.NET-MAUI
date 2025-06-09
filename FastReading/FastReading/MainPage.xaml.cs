e863ed705b2818e14b2d54d8d7c6fd449ce2ef67
EgorMak1 <makaroff.12@yandex.ru>
2025-06-09 11:24:37 +0700

    Добавлена главная страница приложения

--- FastReading/FastReading/MainPage.xaml.cs
+++ FastReading/FastReading/MainPage.xaml.cs
@@ -1,25 +1,39 @@
-﻿namespace FastReading
+﻿using Microsoft.Maui.Controls;
+
+namespace FastReading
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        // Обработчик для кнопки "Начать тренировку"
+        private async void OnStartTrainingClicked(object sender, EventArgs e)
         {
-            count++;
+            // Логика для начала тренировки
+            await DisplayAlert("Начать тренировку", "Вы начали тренировку!", "OK");
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            // Можно добавить переход на экран тренировки, если он будет:
+            // await Navigation.PushAsync(new TrainingPage());
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        // Обработчик для кнопки "Статистика"
+        private async void OnViewStatisticsClicked(object sender, EventArgs e)
+        {
+            // Логика для отображения статистики
+            await DisplayAlert("Статистика", "Здесь будет ваша статистика.", "OK");
+
+            // Также, если есть экран статистики:
+            // await Navigation.PushAsync(new StatisticsPage());
+        }
+
+        // Обработчик для кнопки "Выйти"
+        private void OnExitClicked(object sender, EventArgs e)
+        {
+            // Логика для выхода из приложения
+            System.Environment.Exit(0);
         }
     }
-
 }
